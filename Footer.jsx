9c0a8b884ef230dbdcb1fc20b869e3f38270591d
Analthakkudooz <57003703+Analthakkudooz@users.noreply.github.com>
2021-06-26 17:52:08 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>Copyright by Browser@ {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
